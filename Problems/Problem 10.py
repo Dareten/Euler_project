8c5d99ae7a7593c1950d303ec86b15c9180d1aea
Dareten <daretens@gmail.com>
2020-02-02 20:14:10 +0300

    Добавлено 10 решение

---
+++ Problems/Problem 10.py
@@ -0,0 +1,11 @@
+import timeit
+from math import sqrt
+
+a = timeit.default_timer()
+n = 2000000
+s = set(range(3, n, 2))
+for i in range(2, int(sqrt(n))):
+    if i in s:
+        s -= set(range(i ** 2, n, i))
+print(sum(s) + 2)
+print(timeit.default_timer() - a, end='')
