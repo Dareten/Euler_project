e39431df5b4b47c3172a99b989a76add42110586
Dareten <daretens@gmail.com>
2020-02-02 18:22:04 +0300

    Добавлено 9 решение Решение упакованы в папку

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from subprocess import Popen, PIPE
 
 def switch():
     x = int(input("Выберите номер проблемы: "))
-    s = "python \"./Problem %d.py\"" % x
+    s = "python \"./Problems/Problem %d.py\"" % x
     proc = Popen(
         s,
         shell=True,
