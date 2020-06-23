5abf00f3ddfc8d6873c1633375a6703b526c63b0
Dareten <30283450+Dareten@users.noreply.github.com>
2020-06-23 19:15:29 +0300

    Update Problem 2.py

--- Problems/Problem 2.py
+++ Problems/Problem 2.py
@@ -1,10 +1,6 @@
-import timeit
-
-a = timeit.default_timer()
 f, i = [1, 2], 2
 while f[-1] < 4000000:
     f.append(f[i - 1] + f[i - 2])
     i += 1
 res = sum(filter(lambda x: x % 2 == 0 and x < 4000000, f))
 print(res)
-print(timeit.default_timer() - a, end='')
