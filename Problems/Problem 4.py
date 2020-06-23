b4a9043445818c08bdee9d9a08321b1a17c19b29
Dareten <30283450+Dareten@users.noreply.github.com>
2020-06-23 19:15:48 +0300

    Update Problem 4.py

--- Problems/Problem 4.py
+++ Problems/Problem 4.py
@@ -1,12 +1,6 @@
-import timeit
-
-
-a = timeit.default_timer()
 q, s = False, []
 for i in range(999, 99, -1):
     for j in range(i, 99, -1):
         if i * j == int(str(i * j)[::-1]):
             s.append(i * j)
 print(max(s))
-print(timeit.default_timer() - a, end='')
-
