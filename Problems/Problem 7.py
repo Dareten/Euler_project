93d7b797f248065a1f199516f042c4e578a9de80
Dareten <30283450+Dareten@users.noreply.github.com>
2020-06-24 01:32:23 +0300

    Update Problem 7.py

--- Problems/Problem 7.py
+++ Problems/Problem 7.py
@@ -1,8 +1,5 @@
-import timeit
 from config import issimple
 
-
-a = timeit.default_timer()
 n = 5
 s = [2, 3]
 while True:
@@ -12,4 +9,3 @@ while True:
         break
     n += 2
 print(s[-1])
-print(timeit.default_timer() - a, end='')
